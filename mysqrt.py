b2629439bae389b0e05d7418596343ecfdb442a2
JerryYanWan <shellingwan@gmail.com>
2015-11-16 03:40:34 +0800

    Increasing iteration to 100 times, detect convergence

--- mysqrt.py
+++ mysqrt.py
@@ -1,8 +1,13 @@
 x = 10.
 s = 1.
-
-for k in range(6):
+kmax = 100
+tol = 1e-14
+for k in range(kmax):
     print "Before iteration %d, s = %20.15f" % (k, s)
+    s0 = s
     s = 0.5 * (s + x/s)
+    delta_s = s - s0
+    if abs(delta_s / x) < tol:
+        break
 
 print "After %d iteration,  s = %20.15f" %(k+1, s)
